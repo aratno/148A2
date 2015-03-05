cdb0f6ef281f848419e662b6d94aa0b73bb8879f
kasrakoushan <kasra.koushan@gmail.com>
2015-03-05 16:15:06 -0500

    Implemented doctests

--- tippy_game_state.py
+++ tippy_game_state.py
@@ -7,87 +7,87 @@ from copy import deepcopy
 class TippyGameState(GameState):
     ''' The state of the Tippy game
     
-    current_state is nxn array of current board
+    current_state: list   --- n x n array of current board
     '''
     
     PLAYER = {'p1': 'X', 'p2': 'O'}
     
     def __init__(self, p, interactive=False, \
-                 current_state=[[' ' for x in range(4)] for x in range(4)]):
-        '''
-        (TippyGameState, str, list) --> Nonetype
+                 current_state=[[' ' for i in range(3)] for i in range(3)]):
+        ''' (TippyGameState, str, list) -> Nonetype
         
-        Initializes TippyGameState with current_state as the n by n grid
+        Initialize TippyGameState self with current_state as the n x n grid.
         
-        Assume p is in {'p1','p2'}
-               that len(current_state) > 3 so the game can be won
+        Assume:   p in {'p1','p2'}
+                  current_state is a square list of lists
+                  len(current_state) > 2 so the game can be won
         '''
         
         if interactive:
             size = int(input('Enter the size of your grid: '))
-            if isinstance(size, int) and size >= 3:
-                current_state = [[' ' for x in range(size)] \
-                                 for x in range(size)]
-            else:
-                raise Exception('Invalid grid size.')
+            while size < 3:
+                size = int(input('Size must be at least 3. Try again: '))
+            current_state = [[' ' for i in range(size)] for i in range(size)]
             
         GameState.__init__(self, p)
         self.current_state = current_state
-        self.instructions = 'Enter the row number and then the column \
-number of the location where you wish to make your move. The \
-objective is to make a tippy.'
+        self.over = self.winner('p1') or self.winner('p2') or not \
+            self.possible_next_moves()
+        self.instructions = ('Enter the row number and then the column ' 
+                             'number of the location where you wish to '
+                             'make your move. The objective is to make '
+                             'a tippy.')
         
     def __repr__(self):
-        '''
-        (TippyGameState) --> str
+        ''' (TippyGameState) -> str
         
-        Returns the sring representation of TippyGameState that evaluates to 
-        to an equivalent SubtractSquareState
+        Return a string representation of TippyGameState self that evaluates 
+        to  an equivalent TippyGameState.
         
         >>> t = TippyGameState('p1')
         >>> t
-        TippyGameState('p1', [[' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], \
-        [' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ']])
+        TippyGameState('p1', [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']])
         '''
         
-        return 'TippyGameState({}, {})'.format(repr(self.next_player), \
+        return 'TippyGameState({}, {})'.format(repr(self.next_player),
                                                repr(self.current_state))
     
     def __str__(self):
+        ''' (TippyGameState) -> str
+        
+        Return a convenient string representation of TippyGameState self.
+        
+        >>> t = TippyGameState('p1')
+        >>> print(t)
+        Current board:
+         | | 
+        -----
+         | | 
+        -----
+         | | 
+        <BLANKLINE>
+        Next to play: p1
         '''
-        (TippyGameState) --> str
         
-        Returns a grid resembling what the tippy game should look like
-        
-        >>>t = TippyGameState('p1')
-        >>>print(t)
-         | | | 
-        -------
-         | | | 
-        -------
-         | | | 
-        -------
-         | | | 
-        '''
-        
+        size = len(self.current_state) #assign variable to shorten lines
         grid = ''
-        for i in range(len(self.current_state)):#prints row with vertical bars
-            for j in range(len(self.current_state)):
+        for i in range(size): #iterate through rows
+            for j in range(size): #print row with vertical bars
                 grid += self.current_state[i][j]
-                if j != len(self.current_state) - 1:
+                if j != size - 1:
                     grid += '|'
             
             grid += '\n'
-            if i != len(self.current_state) - 1: #prints line inbetween rows
-                for k in range(2*len(self.current_state) - 1):
+            if i != size - 1: 
+                for k in range(2*size - 1): #print line in between rows
                     grid += '-'
                 grid += '\n'
                 
-        return grid
+        return 'Current board:\n{}\nNext to play: {}'.format(
+            grid, self.next_player)
     
     def __eq__(self, other):
-        '''
-        (TippyGameState, TippyGameState) -> bool
+        ''' (TippyGameState, TippyGameState) -> bool
 
         Return True iff this TippyGameState is the equivalent to other.
 
@@ -102,75 +102,63 @@ objective is to make a tippy.'
                 self.next_player == other.next_player)
     
     def apply_move(self, move):
-        '''(TippyGameState, TippyeMove) -> TippyGameState
+        ''' (TippyGameState, TippyMove) -> TippyGameState
 
-        Return the new TippyGameState reached by applying move to self
+        Return the new TippyGameState reached by applying move to self.
         
         >>> s1 = TippyGameState('p1')
-        >>> s2 = s1.apply_move(TippyMove([1,1]))
+        >>> s2 = s1.apply_move(TippyMove([1, 1]))
         >>> print(s2)
-        X| | | 
-        -------
-         | | | 
-        -------
-         | | | 
-        -------
-         | | | 
+        Current board:
+        X| | 
+        -----
+         | | 
+        -----
+         | | 
+        <BLANKLINE>
+        Next to play: p2
         '''
         
-        new_state = deepcopy(self.current_state)
-        
         if move in self.possible_next_moves():
+            new_state = deepcopy(self.current_state)
             if self.next_player == 'p1':
                 new_state[move.pos[0]][move.pos[1]] = 'X'                
             else:
                 new_state[move.pos[0]][move.pos[1]] = 'O'
+            return TippyGameState(self.opponent(), False, new_state)
         else:
-            raise Exception('Not a valid move.')
-        
-        new = TippyGameState(self.opponent(), False, new_state)
-        
-        new.over =  new.winner('p1') or new.winner('p2') or not \
-            new.possible_next_moves()
-        
-        return new
+            return None
             
     def get_move(self):
-        '''
-        (TippyGameState) -> TippyMove
+        ''' (TippyGameState) -> TippyMove
 
         Prompt user and return move.
         
-        >>>t = TippyGameState('p1')
-        >>>t.get_move()
+        >>> t = TippyGameState('p1')
+        >>> t.get_move()
         Enter the row: 2
         Enter the column: 2
         TippyMove([1, 1])
         '''
         
         move = []
-        #first row taken as 1, then corrected in TippyMove
-        move.append(int(input('Enter the row: ')))
-        move.append(int(input('Enter the column: ')))
+        #first row or column taken as 1, then corrected in TippyMove
+        
+        move.append(int(input('Enter the row (1 to n): ')))
+        move.append(int(input('Enter the column (1 to n): ')))
         
         return TippyMove(move)
     
     def possible_next_moves(self):
-        '''
-        (TippyGameState) -> list of TippyMove
+        ''' (TippyGameState) -> list of TippyMove
 
-        Return a (possibly empty) list of moves that are legal
-        from the present state.
+        Return a (possibly empty) list of moves that are legal from the 
+        present state.
+        
         >>> s1 = TippyGameState('p1')
         >>> s2 = s1.apply_move(TippyMove([1,1]))
-        >>> s2.list_possible_next_moves()
-        [TippyMove([0, 0]), TippyMove([0, 1]), TippyMove([0, 2]), \
-        TippyMove([0, 3]), TippyMove([1, 0]), TippyMove([1, 1]), \
-        TippyMove([1, 2]), TippyMove([1, 3]), TippyMove([2, 0]), \
-        TippyMove([2, 1]), TippyMove([2, 2]), TippyMove([2, 3]), \
-        TippyMove([3, 0]), TippyMove([3, 1]), TippyMove([3, 2]), \
-        TippyMove([3, 3])]
-
+        >>> s2.possible_next_moves()
+        [TippyMove([1, 2]), TippyMove([1, 3]), TippyMove([2, 1]), TippyMove([2, 2]), TippyMove([2, 3]), TippyMove([3, 1]), TippyMove([3, 2]), TippyMove([3, 3])]
         '''
         
         moves = []
@@ -179,90 +167,101 @@ objective is to make a tippy.'
                 for j in range(len(self.current_state)):
                     if self.current_state[i][j] == ' ':
                         moves.append(TippyMove([i + 1, j + 1]))
+                        # the TippyMove representation starts at 1
         
         return moves
     
     def winner(self, player):
-        '''
-        (TippyGameState,str) --> bool
+        ''' (TippyGameState, str) -> bool
         
-        Returns True if a tippy has been formed and player has won
+        Return True iff a tippy has been formed in self and player has won.
         
         >>> s1 = TippyGameState('p1')
         >>> s2 = s1.apply_move(TippyMove([1,1]))
         >>> s2.winner('p1)
         False
+        
+        Precondition: player is either 'p1' or 'p2'
         '''
         
         win = False
         
         i = 0        
-        while not win and i < len(self.current_state):
+        while not win and i < len(self.current_state): #iterate through rows
             j = 0
-            while not win and j < len(self.current_state):
-                win = find_tippy(self.current_state, \
-                                 TippyGameState.PLAYER[player], \
-                                 [i, j])
+            while not win and j < len(self.current_state): #iterate thru cols
+                win = find_tippy(self.current_state,
+                                 TippyGameState.PLAYER[player], [i, j])
+                #determine if tippy has been formed at this position
                 j += 1
             i += 1
-                
         return win
     
     
     def rough_outcome(self):
-        '''
-        (TippyGamestate) --> float
+        ''' (TippyGamestate) -> float
         
-        Returns and estimate in the interval [LOSE,WIN] that gives number of
-        potential winning configs for each player. If game is over, returns 0.0
+        Return an estimate in interval [LOSE,WIN] of best outcome next_player
+        can obtain from state self, by finding how many moves result in wins
+        and losses. If game is over, return 0.0.
         
-        s1 = TippyGameState('p1')
-
-        s2 = s1.apply_move(TippyMove([1,1]))
-        s2.rough_outcome()
-        'Wins: 0.0, Losses: 0.0, Number: 0'
+        >>> s1 = TippyGameState('p1')
+        >>> s2 = s1.apply_move(TippyMove([1,1]))
+        >>> s2.rough_outcome()
+        0.0
         '''
         
         new_states = [self.apply_move(i) for i in self.possible_next_moves()]
-        outcome = [i.winner(self.next_player) for i in new_states]
+        #run through all options (for next_player) and produce new states
+        win_outcome = [i.winner(self.next_player) for i in new_states]
+        #determine which of these are winning states (for next_player)
         
         opp = TippyGameState(self.opponent(), False, self.current_state)
+        #create new state with the opponent next to play
         new_opp = [opp.apply_move(i) for i in opp.possible_next_moves()]
+        #run through all options (for opponent) and produce new states
         opp_outcome = [i.winner(opp.next_player) for i in new_opp]
+        #determine which of these are losing states (for next_player)
         
         wins = float(len([i for i in outcome if i]))
         losses = float(len([i for i in opp_outcome if i]))
         
         try:
-            num = 2*wins/(wins + losses) - 1
+            num = 2*wins/(wins + losses) - 1 #scale value into [LOSE, WIN]
         except ZeroDivisionError:
-            num = 0
+            num = 0.0
         
-        return 'Wins: {}, Losses: {}, Number: {}'.format(wins, losses, num)
+        return num
         
 
 def find_tippy(state, sym, pos):
-    '''
-    (list,str,list) --> bool
+    ''' (list, str, list) -> bool
     
-    returns True if a Tippy has been formed starting at position pos
+    Return True if a tippy made up of letter sym has been formed in state
+    starting at position pos.
+    Helper function for TippyGameState.winner().
     
-    >>>s1 = TippyGameState('p1')
-    >>>temp = s1.current_state
-    >>>temp[0][0] = 'X'
-    >>>temp[0][1] = 'X'
-    >>>temp[1][1] = 'X'
-    >>>temp[1][2] = 'X'
-    >>>find_tippy(temp,'X',[0,0])
+    >>> s1 = TippyGameState('p1')
+    >>> temp = s1.current_state
+    >>> temp[0][0] = 'X'
+    >>> temp[0][1] = 'X'
+    >>> temp[1][1] = 'X'
+    >>> temp[1][2] = 'X'
+    >>> find_tippy(temp, 'X', [0, 0])
     True
+    
+    Precondition: sym is either 'X' or 'O'
     '''
     
     x, y = pos[0], pos[1]
     
     tippy = False
     
+    #because we are iterating from the top left to the bottom right,
+    #there are only four tippy configurations we need to check
+    
     if state[x][y] == sym:
-        try:
+        try: #search for right-rightdown-rightrightdown tippy
             if all([state[x][y + 1] == sym, \
                     state[x + 1][y + 1] == sym, \
                     state[x + 1][y + 2] == sym]):
@@ -270,7 +269,7 @@ def find_tippy(state, sym, pos):
         except IndexError:
             pass
         
-        try:            
+        try: #search for right-down-leftdown tippy       
             if all([state[x][y + 1] == sym, \
                      state[x + 1][y] == sym, \
                      state[x + 1][y - 1] == sym]):
@@ -278,7 +277,7 @@ def find_tippy(state, sym, pos):
         except IndexError:
             pass
         
-        try:
+        try: #search for down-rightdown-rightdowndown tippy
             if all([state[x + 1][y] == sym, \
                      state[x + 1][y + 1] == sym, \
                      state[x + 2][y + 1] == sym]):
@@ -286,7 +285,7 @@ def find_tippy(state, sym, pos):
         except IndexError:
             pass
         
-        try:        
+        try: #search for down-leftdown-leftdowndown tippy
             if all([state[x + 1][y] == sym, \
                      state[x + 1][y - 1] == sym, \
                      state[x + 2][y - 1] == sym]):
@@ -295,3 +294,7 @@ def find_tippy(state, sym, pos):
             pass
     
     return tippy
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
--- tippy_move.py
+++ tippy_move.py
@@ -1,15 +1,15 @@
 from move import Move
 
 class TippyMove(Move):
-    ''' a move in the game of Tippy
+    ''' A move in the game of Tippy.
     
-    pos: a list denoting the row and column of the move on the grid
+    pos: list -- list denoting the row and column of the move on the grid.
     '''
     
     def __init__(self, pos):
         ''' (TippyMove, list) -> NoneType
     
-        Initialize a new TippyMove for removing amount from value.
+        Initialize a new TippyMove for inserting 'X' or 'O' on the grid.
 
         Assume: pos is a list with integer values 
         '''
@@ -18,21 +18,24 @@ class TippyMove(Move):
         #user will enter number from 1 to n
         
     def __repr__(self):
-        '''(TippyMove)--> str
+        ''' (TippyMove) -> str
         
-        Returns the string representation of TippyMove
-        >>>p = TippyMove([1,1])
-        >>>p 
-        TippyMove([1,1])
+        Return the string representation of this TippyMove.
+        
+        >>> p = TippyMove([1, 1])
+        >>> p 
+        TippyMove([1, 1])
         '''
-        return 'TippyMove({})'.format(str(self.pos))
+        return 'TippyMove([{}, {}])'.format(str(self.pos[0] + 1), 
+                                            str(self.pos[1] + 1))
         
     def __str__(self):
-        '''(TippyMove)--> str
+        ''' (TippyMove) -> str
         
-        Returns the string representation of TippyMove that is readable
-        >>>p = TippyMove([1,1])
-        >>>print(p) 
+        Returns the string representation of this TippyMove that is readable.
+        
+        >>> p = TippyMove([1, 1])
+        >>> print(p) 
         Row: 1  Column: 1
         '''
         return 'Row: {}  Column: {}'.format(self.pos[0] + 1,self.pos[1] + 1) 
@@ -42,10 +45,17 @@ class TippyMove(Move):
 
         Return True iff this TippyMove is the same as other.
 
-        >>> m1 = TippyMove([1,2])
-        >>> m2 = TippyMove([2,1])
+        >>> m1 = TippyMove([1, 2])
+        >>> m2 = TippyMove([2, 1])
         >>> print(m1 == m2)
         False
+        >>> m3 = TippyMove([1, 2])
+        >>> print(m1 == m3)
+        True
         '''
         return (isinstance(other, TippyMove) and 
-                        self.pos == other.pos)
+                self.pos == other.pos)
+    
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
